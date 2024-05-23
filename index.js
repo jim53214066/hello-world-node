b6615c81e47be3c31225e1fc8444530a253e4bfc
Jim Tang <jim.tang@refinitiv.com>
2024-05-23 21:56:54 +0800

    default port to 80

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
